f4e972ed1f27dc19fb0699859da08d1278983659
hasanslayer <hasan-dr2010@hotmail.com>
2021-12-22 10:12:01 +0200

    Add product entity.

---
+++ API/Entities/Product.cs
@@ -0,0 +1,9 @@
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string NameAr { get; set; } = "";
+        public string NameEn { get; set; } = "";
+    }
+}
