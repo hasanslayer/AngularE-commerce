cccffd2886e460c7a37ebcfc9e86990d6e10f83b
hasanslayer <hasan-dr2010@hotmail.com>
2022-01-11 23:53:22 +0200

    Add custom value resolver to automapper.

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -12,7 +12,8 @@ namespace API.Helpers
             CreateMap<Product, ProductToReturnDto>()
                             .ForMember(dst => dst.Name, opt => opt.MapFrom(src => Lang == "ar" ? src.NameAr : src.NameEn))
                             .ForMember(dst => dst.ProductBrand, opt => opt.MapFrom(src => Lang == "ar" ? src.ProductBrand.NameAr : src.ProductBrand.NameEn))
-                            .ForMember(dst => dst.ProductType, opt => opt.MapFrom(src => Lang == "ar" ? src.ProductType.NameAr : src.ProductType.NameEn));
+                            .ForMember(dst => dst.ProductType, opt => opt.MapFrom(src => Lang == "ar" ? src.ProductType.NameAr : src.ProductType.NameEn))
+                            .ForMember(dst => dst.ImgUrl, opt => opt.MapFrom<ProductUrlResolver>());
         }
     }
 }
---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,24 @@
+using API.Core.Entities;
+using API.Dtos;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        private readonly IConfiguration _config;
+        public ProductUrlResolver(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.ImgUrl))
+            {
+                return _config["ApiUrl"] + source.ImgUrl;
+            }
+            return null;
+        }
+    }
+}
