f3467ac0430a8aedaa43fb0050c35ada37e22696
hasanslayer <hasan-dr2010@hotmail.com>
2022-02-07 01:23:56 +0200

    Setting up nav links.

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,10 +4,11 @@ import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule,RouterModule],
   exports: [ShopComponent],
 })
 export class ShopModule {}
