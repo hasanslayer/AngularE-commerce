f6cf36a59c82cce757c963fb6344a5d28e9f0e93
hasanslayer <hasan-dr2010@hotmail.com>
2022-02-09 00:22:17 +0200

    Implement product detials component.

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
+  product: IProduct = {
+    id: 0,
+    description: '',
+    imgUrl: '',
+    name: '',
+    price: 0,
+    productBrand: '',
+    productType: '',
+  };
 
-  constructor() { }
+  constructor(
+    private shopService: ShopService,
+    private activeRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.loadProduct();
   }
 
+  loadProduct() {
+    this.shopService.getProduct(Number(this.activeRoute.snapshot.paramMap.get('id'))).subscribe({
+      next: (product) => {
+        this.product = product;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -6,6 +6,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root',
@@ -46,6 +47,11 @@ export class ShopService {
         })
       );
   }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
+  }
+
   getTypes(): Observable<IType[]> {
     return this.http.get<IType[]>(this.baseUrl + 'products/types');
   }
