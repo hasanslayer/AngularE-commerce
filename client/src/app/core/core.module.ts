c682d37f1055b40ae4318329674a5cabba7be49b
hasanslayer <hasan-dr2010@hotmail.com>
2022-02-12 20:35:07 +0200

    Add toast notification.

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -5,10 +5,23 @@ import { RouterModule } from '@angular/router';
 import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
-  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
-  imports: [CommonModule,RouterModule],
+  declarations: [
+    NavBarComponent,
+    TestErrorComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
+  ],
   exports: [NavBarComponent],
 })
 export class CoreModule {}
--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -7,29 +7,34 @@ import {
 } from '@angular/common/http';
 import { catchError, Observable, throwError } from 'rxjs';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private router: Router) {
-
-  }
+  constructor(private router: Router, private toastr: ToastrService) {}
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
-      catchError(error => {
-        if(error){
-          if(error.status === 404){
-            this.router.navigateByUrl('/not-found')
+      catchError((error) => {
+        if (error) {
+          if (error.status === 400) {
+            this.toastr.error(error.error.message,error.error.statusCode)
           }
-          if(error.status === 500){
-            this.router.navigateByUrl('/server-error')
+          if (error.status === 401) {
+            this.toastr.error(error.error.message,error.error.statusCode)
+          }
+          if (error.status === 404) {
+            this.router.navigateByUrl('/not-found');
+          }
+          if (error.status === 500) {
+            this.router.navigateByUrl('/server-error');
           }
         }
         return throwError(() => new Error('test from interceptors'));
       })
-    )
+    );
   }
 }
