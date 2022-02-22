96a914bd2135994e485b63c4df73daef37b68857
hasanslayer <hasan-dr2010@hotmail.com>
2022-02-22 23:28:49 +0200

    prepare for using Redis.

--- API/Program.cs
+++ API/Program.cs
@@ -4,6 +4,7 @@ using API.Infrastructure.Data;
 using API.Middleware;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,12 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSingleton<ConnectionMultiplexer>(c => {
+    var configuration = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis"),true);
+
+    return ConnectionMultiplexer.Connect(configuration);
+});
 
 
 
