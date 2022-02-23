164c0268311aa3082183708fa493cb91bb4ff151
hasanslayer <hasan-dr2010@hotmail.com>
2022-02-23 22:41:34 +0200

    Add cart module, routing and service.

---
+++ client/src/app/cart/cart-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart.component';
+
+
+const routes: Routes = [
+  {path:'',component:CartComponent}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports:[RouterModule]
+})
+export class CartRoutingModule { }
