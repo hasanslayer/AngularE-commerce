35b69dccdad1d4456266481781ef61d92acf484c
hasanslayer <hasan-dr2010@hotmail.com>
2022-03-01 00:08:03 +0200

    Add cart component function.

--- client/src/app/cart/cart.component.ts
+++ client/src/app/cart/cart.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ICart } from '../shared/models/cart';
+import { ICart, ICartItem } from '../shared/models/cart';
 import { CartService } from './cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
+  cart$!: Observable<ICart>;
 
-  cart$!:Observable<ICart>;
-
-  constructor(private cartService:CartService) { }
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
     this.cart$ = this.cartService.cart$;
   }
 
+  removeCartItem(item: ICartItem) {
+    this.cartService.removeItemFromCart(item);
+  }
+
+  incrementQty(item: ICartItem) {
+    this.cartService.incrementItemQty(item);
+  }
+
+  decrementQty(item: ICartItem) {
+    this.cartService.decrementItemQty(item);
+  }
 }
