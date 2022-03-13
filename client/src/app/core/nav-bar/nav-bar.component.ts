b9ee9eeeaf7415e6258bf53a5dd4a7164d2c90e1
hasanslayer <hasan-dr2010@hotmail.com>
2022-03-13 22:46:24 +0200

    Updating navbar for the logged in user.

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AccountService } from 'src/app/account/account.service';
 import { CartService } from 'src/app/cart/cart.service';
 import { ICart } from 'src/app/shared/models/cart';
+import { IUser } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,11 +12,13 @@ import { ICart } from 'src/app/shared/models/cart';
 })
 export class NavBarComponent implements OnInit {
   cart$!: Observable<ICart>;
+  currentUser$!: Observable<IUser>;
 
-  constructor(private cartService:CartService) { }
+  constructor(private cartService:CartService,private accountService:AccountService) { }
 
   ngOnInit(): void {
      this.cart$ = this.cartService.cart$;
+     this.currentUser$ = this.accountService.currentUser$;
   }
 
 }
