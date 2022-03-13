76ecd43e80a9467f40a15855961a30f26dd5ca56
hasanslayer <hasan-dr2010@hotmail.com>
2022-03-13 22:27:29 +0200

    Using reactive forms.

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm!: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.createLoginForm();
   }
 
+  createLoginForm() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('',Validators.required)
+    });
+  }
+
+  onSubmit(){
+    console.log(this.loginForm.value);
+  }
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -5,16 +5,18 @@ import { PagingHeaderComponent } from './components/paging-header/paging-header.
 import { PagerComponent } from './components/pager/pager.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
-  imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
+  imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot(),ReactiveFormsModule],
   exports: [
     PaginationModule,
     PagingHeaderComponent,
     PagerComponent,
     CarouselModule,
-    OrderTotalsComponent
+    OrderTotalsComponent,
+    ReactiveFormsModule
   ],
 })
 export class SharedModule {}
