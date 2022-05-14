5bbfbb3dbccf818a4fdab3093844124bfa10d250
hasanslayer <hasan-dr2010@hotmail.com>
2022-05-14 14:37:38 +0200

    Create delivery form.

--- client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
+++ client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { IDeliveryMethod } from 'src/app/shared/models/deliveryMethod';
+import { CheckoutService } from '../checkout.service';
 
 @Component({
   selector: 'app-checkout-delivery',
   templateUrl: './checkout-delivery.component.html',
-  styleUrls: ['./checkout-delivery.component.scss']
+  styleUrls: ['./checkout-delivery.component.scss'],
 })
 export class CheckoutDeliveryComponent implements OnInit {
+  @Input() checkoutForm!: FormGroup;
+  deliveryMethods: IDeliveryMethod[] = [];
 
-  constructor() { }
+  constructor(private checkoutService: CheckoutService) {}
 
   ngOnInit(): void {
+    this.checkoutService
+      .getDeliveryMethods()
+      .subscribe({
+        next:(dm:IDeliveryMethod[]) => {
+          this.deliveryMethods = dm;
+        },
+        error: (e) => console.log(e)
+      });
   }
-
 }
--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { IDeliveryMethod } from '../shared/models/deliveryMethod';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CheckoutService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getDeliveryMethods() {
+    return this.http
+      .get<IDeliveryMethod[]>(this.baseUrl + 'orders/deliveryMethods')
+      .pipe(
+        map((dm: IDeliveryMethod[]) => {
+          return dm.sort((a, b) => b.price - a.price); // highest price first
+        })
+      );
+  }
 }
