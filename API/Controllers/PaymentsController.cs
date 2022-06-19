87e5f68d6366e630640c54665e529ea39f793738
hasanslayer <hasan-dr2010@hotmail.com>
2022-06-19 23:47:59 +0200

    Creating payment controller.

---
+++ API/Controllers/PaymentsController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    public class PaymentsController : BaseApiController
+    {
+        private readonly IPaymentService _paymentService;
+        public PaymentsController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [Authorize]
+        [HttpPost("{cartId}")]
+        public async Task<ActionResult<Cart>> UpdatePaymentIntent(string cartId)
+        {
+            return await _paymentService.CreateOrUpdatePaymentIntent(cartId);
+        }
+    }
+}
