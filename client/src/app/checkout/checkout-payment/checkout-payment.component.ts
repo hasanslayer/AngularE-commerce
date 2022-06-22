54b4a5dc4fc2bed9474068bdbd92255760afba9c
hasanslayer <hasan-dr2010@hotmail.com>
2022-06-22 23:49:56 +0200

    Add stripe elements to the component.

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { NavigationExtras, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -7,13 +15,23 @@ import { ICart } from 'src/app/shared/models/cart';
 import { IOrder } from 'src/app/shared/models/order';
 import { CheckoutService } from '../checkout.service';
 
+declare var Stripe: any;
+
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
   styleUrls: ['./checkout-payment.component.scss'],
 })
-export class CheckoutPaymentComponent implements OnInit {
+export class CheckoutPaymentComponent implements AfterViewInit,OnDestroy {
   @Input() checkoutForm!: FormGroup;
+  @ViewChild('cardNumber', { static: true }) cardNumberElement!: ElementRef;
+  @ViewChild('cardExpiry', { static: true }) cardExpiryElement!: ElementRef;
+  @ViewChild('cardCvc', { static: true }) cardCvcElement!: ElementRef;
+  stripe: any; // pure javascript
+  cardNumber: any; // pure javascript
+  cardExpiry: any; // pure javascript
+  cardCvc: any; // pure javascript
+  cardErrors: any; // pure javascript
 
   constructor(
     private cartService: CartService,
@@ -22,7 +40,28 @@ export class CheckoutPaymentComponent implements OnInit {
     private router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.cardNumber.destroy();
+    this.cardExpiry.destroy();
+    this.cardCvc.destroy();
+  }
+
+  ngAfterViewInit(): void {
+    this.stripe = Stripe(
+      'pk_test_51LAhC5EL0awc6qA7wRWTtYs2wrjkKq7OLMESWphQzANGoZ36oUnXErCZtutMIv6mKO0v6Ttn5m10nKLkZJGY5Qwz001v6A3xn6'
+    );
+
+    const elements = this.stripe.elements();
+
+    this.cardNumber = elements.create('cardNumber');
+    this.cardNumber.mount(this.cardNumberElement.nativeElement);
+
+    this.cardExpiry = elements.create('cardExpiry');
+    this.cardExpiry.mount(this.cardExpiryElement.nativeElement);
+
+    this.cardCvc = elements.create('cardCvc');
+    this.cardCvc.mount(this.cardCvcElement.nativeElement);
+  }
 
   submitOrder() {
     const cart = this.cartService.getCurrentCartValue();
