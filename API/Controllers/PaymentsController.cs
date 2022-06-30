19d55c6084c566eb250ef7d81724b4e2873d5078
hasanslayer <hasan-dr2010@hotmail.com>
2022-06-30 15:08:47 +0200

    Start using stripe webhooks.

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -1,22 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Errors;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Stripe;
+using Order = Core.Entities.OrderAggregate.Order;
 
 namespace API.Controllers
 {
     public class PaymentsController : BaseApiController
     {
         private readonly IPaymentService _paymentService;
-        public PaymentsController(IPaymentService paymentService)
+        private const string WhSecret = ""; // we get it from stripe
+        private readonly ILogger<PaymentsController> _logger;
+
+        public PaymentsController(IPaymentService paymentService, ILogger<PaymentsController> logger)
         {
+            _logger = logger;
             _paymentService = paymentService;
         }
 
@@ -26,11 +26,41 @@ namespace API.Controllers
         {
             var cart = await _paymentService.CreateOrUpdatePaymentIntent(cartId);
 
-            if(cart == null){
-                return BadRequest(new ApiResponse(400,"Problem with your cart"));
+            if (cart == null)
+            {
+                return BadRequest(new ApiResponse(400, "Problem with your cart"));
             }
-            
+
             return cart;
         }
+
+        [HttpPost("webhook")]
+        public async Task<ActionResult> StripeWebhook()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signiture"], WhSecret);
+
+            PaymentIntent intent;
+            Order order;
+
+            switch (stripeEvent.Type)
+            {
+                case "payment_intent.succeeded":
+                    intent = (PaymentIntent)stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment Succeeded: ", intent.Id);
+                    // TODO : update order with new status 
+                    break;
+
+                case "payment_intent_failed":
+                    intent = (PaymentIntent)stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment Failed: ", intent.Id);
+                    // TODO : update order status
+                    break;
+            }
+
+            return new EmptyResult(); // to confirm that we received their stripe event
+        }
+
+
     }
 }
