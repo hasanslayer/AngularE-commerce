7f619c3ea35ea84f34e13d24ae6be60a6c72e67a
hasanslayer <hasan-dr2010@hotmail.com>
2022-07-06 01:41:02 +0200

    Caching client - product, brands and types.

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
@@ -13,8 +13,11 @@ import { IProduct } from '../shared/models/product';
 })
 export class ShopService {
   baseUrl = 'https://localhost:7094/api/';
+  products: IProduct[] = [];
+  brands: IBrand[] = [];
+  types: IType[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getProducts(shopParams: ShopParams): Observable<IPagination> {
     let params = new HttpParams();
@@ -43,31 +46,41 @@ export class ShopService {
       })
       .pipe(
         map((response) => {
+          this.products = response.body?.data!;
           return response.body!;
         })
       );
   }
 
   getProduct(id: number) {
+    const product = this.products.find(p => p.id == id);
+
+    if (product) {
+      return of(product);
+    }
     return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getTypes(): Observable<IType[]> {
-    return this.http.get<IType[]>(this.baseUrl + 'products/types');
+    if (this.types.length > 0) {
+      return of(this.types);
+    }
+    return this.http.get<IType[]>(this.baseUrl + 'products/types').pipe(
+      map(response => {
+        this.types = response;
+        return response;
+      })
+    );
   }
   getBrands(): Observable<IBrand[]> {
-<<<<<<< HEAD
     if (this.brands.length > 0) {
       return of(this.brands);
     }
     return this.http.get<IBrand[]>(this.baseUrl + 'products/brands').pipe(
       map(response => {
-        this.brands = response; // cache it first to reuse it from cache instead of get it again from api
+        this.brands = response;
         return response;
       })
     );
-=======
-    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
->>>>>>> parent of 9ce6b4c (Caching client - product, brands and types.)
   }
 }
